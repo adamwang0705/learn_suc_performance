ce54eb2859b38d523498e20875c224dc006c706f
daheng <adamwang0705@gmail.com>
2018-03-10 17:35:55 -0500

    complete version v1.0

--- learn_suc.cpp
+++ learn_suc.cpp
@@ -32,17 +32,17 @@ typedef double real;
 
 /* Item, type, and behavior information */
 lint items_num, types_num, behaviors_num;
-vector<lint> items, types;
-map<lint, lint> item2type, item2item_idx, type2type_idx, item_idx2type_idx;
-vector<vector<lint>> type_idx2item_indices, behaviors;
-vector<real> type_weights;
+vector<lint> items, types, behaviors;
+map<lint, lint> item2type, item2item_idx, type2type_idx, item_idx2type_idx, behavior2behavior_idx;
+vector<vector<lint>> type_idx2item_indices, behavior_idx2item_indices;
+vector<real> type_weights, behavior_rates;
 
 /* Embeddings */
 vector<vector<real>> item_embeddings;
 
 /* Parameters */
 char itemlist_file[MAX_STR_LEN], behaviorlist_file[MAX_STR_LEN], output_file[MAX_STR_LEN],
-        typeweights_file[MAX_STR_LEN] = "";
+        typeweights_file[MAX_STR_LEN] = "", behaviorrates_file[MAX_STR_LEN] = "";
 lint dimensions_num  = 128, threads_num = 8, total_samples_num = (lint)1e6,
         negative_sampling_num = 10, negative_sampling_mode = 1;
 real learning_rate = 0.025;
@@ -121,11 +121,12 @@ void *train_learn_suc_thread(void *) {
         uniform_int_distribution<lint> dist_b_size(0, behaviors_num-1);
         auto sampled_pos_b_idx = dist_b_size(engine);
         vector<lint> pos_b_t_i_counts(static_cast<unsigned long>(types_num));
-        auto pos_b_size = behaviors[sampled_pos_b_idx].size();
+        auto pos_b_size = behavior_idx2item_indices[sampled_pos_b_idx].size();
+        const auto& pos_b_observed_rate = behavior_rates[sampled_pos_b_idx];
 
         /* Compute positive behavior vector (type-weighted sum of item vectors) */
         vector<real> pos_b_vec(static_cast<unsigned long>(dimensions_num));
-        for(const auto& i_idx: behaviors[sampled_pos_b_idx]) {
+        for(const auto& i_idx: behavior_idx2item_indices[sampled_pos_b_idx]) {
             const auto& t_idx = item_idx2type_idx[i_idx];
             const auto& t_w = type_weights[t_idx];
             pos_b_t_i_counts[t_idx] ++;
@@ -141,11 +142,11 @@ void *train_learn_suc_thread(void *) {
 
         /* Compute gradient (with out w_t_(c)) */
         for(lint d=0; d<dimensions_num; ++d) {
-            pos_b_vec[d] *= pos_b_sinh;
+            pos_b_vec[d] *= (pos_b_sinh * pos_b_observed_rate);
         }
 
         /* Update item embeddings */
-        for(const auto& i_idx: behaviors[sampled_pos_b_idx]) {
+        for(const auto& i_idx: behavior_idx2item_indices[sampled_pos_b_idx]) {
             const auto& t_w = type_weights[item_idx2type_idx[i_idx]];
             for(lint d=0; d<dimensions_num; ++d) {
                 item_embeddings[i_idx][d] += pos_b_vec[d] * t_w * curr_learning_rate;
@@ -154,89 +155,90 @@ void *train_learn_suc_thread(void *) {
 
         /*
          * Sample negative behaviors
+         * If only positive behaviors are observed (no behaviorrates_file given)
          * */
-        for (lint n=0; n<negative_sampling_num; ++n) {
-            /* Sample a negative behavior based on given negative sampling strategy */
-            set<lint> neg_b_i_indices;
-            unsigned long cum_neg_b_i_count = 0;
+        if (strlen(behaviorrates_file) == 0) {
+            for (lint n = 0; n < negative_sampling_num; ++n) {
+                /* Sample a negative behavior based on given negative sampling strategy */
+                set<lint> neg_b_i_indices;
+                unsigned long cum_neg_b_i_count = 0;
 
-            if (negative_sampling_mode == 1) {
-                /* Size-constrained negative behavior sampling */
-                // Generate samples number of each item type
-                // Randomly cut positive behavior size into types number parts
-                vector<lint> neg_b_t_cuts(static_cast<unsigned long>(types_num-1));
-                uniform_int_distribution<lint> dist_pos_b_size(0, pos_b_size-1);
-                for (lint c=0; c<types_num-1; ++c) {
-                    neg_b_t_cuts[c] = dist_pos_b_size(engine);
+                if (negative_sampling_mode == 1) {
+                    /* Size-constrained negative behavior sampling */
+                    // Generate samples number of each item type
+                    // Randomly cut positive behavior size into types number parts
+                    vector<lint> neg_b_t_cuts(static_cast<unsigned long>(types_num - 1));
+                    uniform_int_distribution<lint> dist_pos_b_size(0, pos_b_size - 1);
+                    for (lint c = 0; c < types_num - 1; ++c) {
+                        neg_b_t_cuts[c] = dist_pos_b_size(engine);
+                    }
+
+                    sort(neg_b_t_cuts.begin(), neg_b_t_cuts.end());  // Sort cuts ascending
+                    // Count items number between cuts
+                    vector<lint> neg_b_t_i_counts(static_cast<unsigned long>(types_num));
+                    for (lint t = 0; t < types_num - 1; ++t) {
+                        if (t == 0) {
+                            neg_b_t_i_counts[0] = neg_b_t_cuts[0];
+                        } else {
+                            neg_b_t_i_counts[t] = neg_b_t_cuts[t] - neg_b_t_cuts[t - 1];
+                        }
+                    }
+                    neg_b_t_i_counts.back() = pos_b_size - neg_b_t_cuts.back();
+
+                    // Generate random item indices in each type
+                    for (const auto &t: types) {
+                        const auto &type_idx = type2type_idx[t];
+                        const auto &neg_b_t_i_count = neg_b_t_i_counts[type_idx];
+                        cum_neg_b_i_count += neg_b_t_i_count;
+                        // Sample type items without duplicates
+                        uniform_int_distribution<lint> dist_t_i_size(0, type_idx2item_indices[type_idx].size() - 1);
+                        while (neg_b_i_indices.size() < cum_neg_b_i_count) {
+                            const auto &sampled_t_i_idx = type_idx2item_indices[type_idx][dist_t_i_size(engine)];
+                            neg_b_i_indices.insert(sampled_t_i_idx);
+                        }
+                    }
+                } else if (negative_sampling_mode == 2) {
+                    /* Type-distribution constrained negative behavior sampling */
+                    for (const auto &t: types) {
+                        const auto &type_idx = type2type_idx[t];
+                        const auto &pos_b_t_i_count = pos_b_t_i_counts[type_idx];
+                        cum_neg_b_i_count += pos_b_t_i_count;
+                        // Sample type items without duplicates
+                        uniform_int_distribution<lint> dist_t_i_size(0, type_idx2item_indices[type_idx].size() - 1);
+                        while (neg_b_i_indices.size() < cum_neg_b_i_count) {
+                            const auto &sampled_t_i_idx = type_idx2item_indices[type_idx][dist_t_i_size(engine)];
+                            neg_b_i_indices.insert(sampled_t_i_idx);
+                        }
+                    }
+                } else {
+                    cout << "Error: unknown negative sampling mode!" << endl;
+                    exit(1);
                 }
 
-                sort(neg_b_t_cuts.begin(), neg_b_t_cuts.end());  // Sort cuts ascending
-                // Count items number between cuts
-                vector<lint> neg_b_t_i_counts(static_cast<unsigned long>(types_num));
-                for(lint t=0; t<types_num-1; ++t) {
-                    if (t == 0) {
-                        neg_b_t_i_counts[0] = neg_b_t_cuts[0];
-                    } else {
-                        neg_b_t_i_counts[t] = neg_b_t_cuts[t] - neg_b_t_cuts[t-1];
+                /* Compute negative behavior vector (type-weighted sum of item vectors) */
+                vector<real> neg_b_vec(static_cast<unsigned long>(dimensions_num));
+                for (const auto &i_idx: neg_b_i_indices) {
+                    const auto &t_w = type_weights[item_idx2type_idx[i_idx]];
+                    for (lint d = 0; d < dimensions_num; ++d) {
+                        neg_b_vec[d] += item_embeddings[i_idx][d] * t_w;
                     }
                 }
-                neg_b_t_i_counts.back() = pos_b_size - neg_b_t_cuts.back();
 
-                // Generate random item indices in each type
-                for (const auto& t: types) {
-                    const auto& type_idx = type2type_idx[t];
-                    const auto& neg_b_t_i_count = neg_b_t_i_counts[type_idx];
-                    cum_neg_b_i_count += neg_b_t_i_count;
-                    // Sample type items without duplicates
-                    uniform_int_distribution<lint> dist_t_i_size(0, type_idx2item_indices[type_idx].size()-1);
-                    while(neg_b_i_indices.size() < cum_neg_b_i_count) {
-                        const auto& sampled_t_i_idx = type_idx2item_indices[type_idx][dist_t_i_size(engine)];
-                        neg_b_i_indices.insert(sampled_t_i_idx);
+                /* Compute negative behavior norm, gradient coefficient */
+                auto neg_b_norm = get_b_norm(neg_b_vec);
+                auto neg_b_sinh = quick_neg_b_sinh(neg_b_norm);
+
+                /* Compute gradient vector (with out w_t_(c)) */
+                for (lint d = 0; d < dimensions_num; ++d) {
+                    neg_b_vec[d] *= neg_b_sinh;
+                }
+
+                /* Update item embeddings */
+                for (const auto &i_idx: neg_b_i_indices) {
+                    const auto &t_w = type_weights[item_idx2type_idx[i_idx]];
+                    for (lint d = 0; d < dimensions_num; ++d) {
+                        item_embeddings[i_idx][d] -= neg_b_vec[d] * t_w * curr_learning_rate;
                     }
-                }
-            } else if (negative_sampling_mode == 2) {
-                /* Type-distribution constrained negative behavior sampling */
-                for (const auto& t: types) {
-                    const auto& type_idx = type2type_idx[t];
-                    const auto& pos_b_t_i_count = pos_b_t_i_counts[type_idx];
-                    cum_neg_b_i_count += pos_b_t_i_count;
-                    // Sample type items without duplicates
-                    uniform_int_distribution<lint> dist_t_i_size(0, type_idx2item_indices[type_idx].size()-1);
-                    while(neg_b_i_indices.size() < cum_neg_b_i_count) {
-                        const auto& sampled_t_i_idx = type_idx2item_indices[type_idx][dist_t_i_size(engine)];
-                        neg_b_i_indices.insert(sampled_t_i_idx);
-                    }
-                }
-            } else if (negative_sampling_mode == 0) {
-                ;  // No negative sampling, for test only
-            } else {
-                cout << "Negative sampling negative_sampling_mode wrong!" << endl;
-                break;
-            }
-
-            /* Compute negative behavior vector (type-weighted sum of item vectors) */
-            vector<real> neg_b_vec(static_cast<unsigned long>(dimensions_num));
-            for(const auto& i_idx: neg_b_i_indices) {
-                const auto& t_w = type_weights[item_idx2type_idx[i_idx]];
-                for (lint d=0; d<dimensions_num; ++d) {
-                    neg_b_vec[d] += item_embeddings[i_idx][d] * t_w ;
-                }
-            }
-
-            /* Compute negative behavior norm, gradient coefficient */
-            auto neg_b_norm = get_b_norm(neg_b_vec);
-            auto neg_b_sinh = quick_neg_b_sinh(neg_b_norm);
-
-            /* Compute gradient vector (with out w_t_(c)) */
-            for(lint d=0; d<dimensions_num; ++d) {
-                neg_b_vec[d] *= neg_b_sinh;
-            }
-
-            /* Update item embeddings */
-            for(const auto& i_idx: neg_b_i_indices) {
-                const auto& t_w = type_weights[item_idx2type_idx[i_idx]];
-                for(lint d=0; d<dimensions_num; ++d) {
-                    item_embeddings[i_idx][d] -= neg_b_vec[d] * t_w * curr_learning_rate;
                 }
             }
         }
@@ -315,8 +317,8 @@ void read_itemlist_file(const string& itemlist_file, char delim='\t') {
             tokens.push_back(stol(token));  // Parse to lint
         }
         if(tokens.size() != 2) {  // Line format wrong
-            cout << "Input itemlist file format wrong!" << endl;
-            break;
+            cout << "Error: input itemlist file format wrong!" << endl;
+            exit(1);
         } else {
             items.push_back(tokens[0]);
             item2type.insert(make_pair(tokens[0], tokens[1]));
@@ -376,9 +378,10 @@ void read_behaviorlist_file(const string& behaviorlist_file, char delim_l1='\t',
         exit(1);
     }
 
-    /* Populate behaviors */
+    /* Populate behaviors, behavior2behavior_idx, behavior_idx2item_indices */
     set<lint> items_set(items.begin(), items.end());
     bool in_items_set;
+    lint behavior_idx_count = 0;
     for (string line; getline(filein, line); ) {
         vector<string> tokens;
         stringstream ss(line);
@@ -387,20 +390,25 @@ void read_behaviorlist_file(const string& behaviorlist_file, char delim_l1='\t',
             tokens.push_back(token);
         }
         if(tokens.size() != 2) {
-            cout << "Input behaviorlist file format wrong!" << endl;
-            break;
+            cout << "Error: input behaviorlist file format wrong!" << endl;
+            exit(1);
         } else {
+            behaviors.push_back(stol(tokens[0]));
+            behavior2behavior_idx.insert(make_pair(stol(tokens[0]), behavior_idx_count));
+
             vector<lint> tokens_l2;
             stringstream ss_l2(tokens[1]);  // tokens[0] for behavior; tokens[1] for items
             string token_l2;
             while (getline(ss_l2, token_l2, delim_l2)) {
                 /* Discard items not in itemlist file */
-                in_items_set = items_set.find(stoul(token_l2)) != items_set.end();
+                in_items_set = items_set.find(stol(token_l2)) != items_set.end();
                 if(in_items_set) {
-                    tokens_l2.push_back(item2item_idx[stoul(token_l2)]);
+                    tokens_l2.push_back(item2item_idx[stol(token_l2)]);
                 }
             }
-            behaviors.push_back(tokens_l2);
+            behavior_idx2item_indices.push_back(tokens_l2);
+
+            behavior_idx_count ++;
         }
     }
     behaviors_num = behaviors.size();
@@ -411,7 +419,7 @@ void read_behaviorlist_file(const string& behaviorlist_file, char delim_l1='\t',
  * Read in item type weights information from typeweigths file.
  * Each line follows format: <non_neg_int_item_type>\t<pos_float_item_type_weight>
  */
-void read_typeweights_file(const string& typeweights_file, char delim='\t') {
+void read_typeweights_file (const string& typeweights_file, char delim='\t') {
     cout << "Reading typeweights file..." << endl;
     ifstream filein(typeweights_file);
     if (!filein) {
@@ -430,14 +438,51 @@ void read_typeweights_file(const string& typeweights_file, char delim='\t') {
             tokens.push_back(stod(token));  // Parse to real
         }
         if(tokens.size() != 2) {  // Line format wrong
-            cout << "Input typeweights file format wrong!" << endl;
-            break;
+            cout << "Error: input typeweights file format wrong!" << endl;
+            exit(1);
         } else {
             /* Discard types not in itemlist file */
             in_types_set = types_set.find((lint)tokens[0]) != types_set.end();
             if (in_types_set) {
                 const auto& type_idx = type2type_idx[(lint)tokens[0]];
                 type_weights[type_idx] = tokens[1];
+            }
+        }
+    }
+    cout << "Done!" << endl;
+}
+
+/*
+ * Read in positive behavior success rates information from behaviorrates file.
+ * Each line follows format: <non_neg_int_behavior>\t<pos_float_behavior_success_rate>
+ */
+void read_behaviorrates_file (const string& behaviorrates_file, char delim='\t') {
+    cout << "Reading behaviorrates file..." << endl;
+    ifstream filein(behaviorrates_file);
+    if (!filein) {
+        cout << "Error: file of behaviorrates not found!" << endl;
+        exit(1);
+    }
+
+    set<lint> behaviors_set(behaviors.begin(), behaviors.end());
+    bool in_behaviors_set;
+    /* Update behavior_rates */
+    for (string line; getline(filein, line); ) {
+        vector<real> tokens;
+        stringstream ss(line);
+        string token;
+        while (getline(ss, token, delim)) {
+            tokens.push_back(stod(token));  // Parse to real
+        }
+        if(tokens.size() != 2) {  // Line format wrong
+            cout << "Error: input typeweights file format wrong!" << endl;
+            exit(1);
+        } else {
+            /* Discard behaviors not in behaviorlist file */
+            in_behaviors_set = behaviors_set.find((lint)tokens[0]) != behaviors_set.end();
+            if (in_behaviors_set) {
+                const auto& behavior_idx = behavior2behavior_idx[(lint)tokens[0]];
+                behavior_rates[behavior_idx] = tokens[1];
             }
         }
     }
@@ -456,7 +501,6 @@ void initialize() {
     // Default weight scheme: all equal to 1
     real default_weight  = 1.0;
     type_weights = vector<real>(static_cast<unsigned long>(types_num), default_weight);
-
     // If optional typeweights file provided
     if (strlen(typeweights_file) != 0) {
         read_typeweights_file(typeweights_file);
@@ -467,6 +511,15 @@ void initialize() {
         cout << t << "-" << type_weights[type_idx] << " ";
     }
     cout << endl;
+
+    /* Positive behavior success rates */
+    // Default behavior success rate is 1
+    real default_rate  = 1.0;
+    behavior_rates = vector<real>(static_cast<unsigned long>(behaviors_num), default_rate);
+    // If optional behaviorrates file provided
+    if (strlen(behaviorrates_file) != 0) {
+        read_behaviorrates_file(behaviorrates_file);
+    }
 
     cout << "Initializing..." << endl;
     /* Item embeddings */
@@ -545,6 +598,7 @@ int main(int argc, char **argv) {
     if ((i = parse_args((char *)"--threads", argc, argv)) > 0) threads_num = stol(argv[i + 1]);
     // Optional
     if ((i = parse_args((char *)"--typeweights", argc, argv)) > 0) strcpy(typeweights_file, argv[i + 1]);
+    if ((i = parse_args((char *)"--behaviorrates", argc, argv)) > 0) strcpy(behaviorrates_file, argv[i + 1]);
 
     cout << "Arguments:" << endl
          << " --itemlist: " << itemlist_file << endl
@@ -558,6 +612,9 @@ int main(int argc, char **argv) {
          << " --threads: " << threads_num << endl;
     if (strlen(typeweights_file) != 0) {
         cout << " --typeweights: " << typeweights_file << endl;
+    }
+    if (strlen(behaviorrates_file) != 0) {
+        cout << " --behaviorrates: " << behaviorrates_file << endl;
     }
 
     cout << "====================================================" << endl;
