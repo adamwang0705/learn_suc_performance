874520e08db38d269e2f3339669d8065e0780dca
daheng <adamwang0705@gmail.com>
2018-03-22 15:59:48 -0400

    complete readme

--- train.sh
+++ train.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+make clean
 make all
 
-## Default settings
-# ./learn_suc --itemlist data/itemlist.txt --behaviorlist data/behaviorlist.txt --output learn_suc-default.txt
+## Default options
+# ./learn_suc --itemlist data/itemlist.txt --behaviorlist data/behaviorlist.txt --output learn_suc.txt
 
-## Complete settings
-./learn_suc --itemlist data/itemlist.txt --behaviorlist data/behaviorlist.txt --output learn_suc-m1.txt --dim 128 --mode 1 --samples 1 --negative 10 --rate 0.025 --threads 8
-
-make clean
+## Complete options
+./learn_suc --itemlist data/itemlist.txt --behaviorlist data/behaviorlist.txt --output learn_suc.tw.txt --dim 128 --mode 1 --samples 1 --negative 10 --rate 0.025 --threads 8 --typeweights data/typeweights.txt
